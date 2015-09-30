17781d9b417c33a42d69a82f2e05c7cb0cd80ed2
Adler <nkj20932@hotmail.com>
2015-10-01 00:41:05 +0800

    fix Router documentation

--- src/iceberg/router.cr
+++ src/iceberg/router.cr
@@ -1,17 +1,15 @@
-Map = {} of String => String
-
-# User can define app routes using Iceberg::Router.
-# 
-# ```crystal
-# Iceberg::Router.draw do |route|
-#   get "/", :app
-#   get "/show", "app#show"
-# end
-# 
-# :app will look for the default "index" action under AppController
-# "app#show" will look for the "show" action under AppController 
-# ```
 module Iceberg
+  # User can define app routes using Iceberg::Router.
+  # 
+  # ```crystal
+  # Iceberg::Router.draw do |route|
+  #   get "/", :app
+  #   get "/show", "app#show"
+  # end
+  # 
+  # :app will look for the default "index" action under AppController
+  # "app#show" will look for the "show" action under AppController 
+  # ```
   class Router
 
     def self.draw
@@ -25,7 +23,17 @@ module Iceberg
       {% else %}
         action = {{input.capitalize.id}}Controller.new.index
       {% end %}
-      Map[{{path}}] = action
+      Map::Get[{{path}}] = action
+    end
+
+    macro post(path, input)
+      {% if input.is_a?(StringLiteral) && input.split("#").size == 2 %}
+        {% split = input.split("#") %}
+        action = {{split[0].capitalize.id}}Controller.new.{{split[1].id}}
+      {% else %}
+        action = {{input.capitalize.id}}Controller.new.index
+      {% end %}
+      Map::Post[{{path}}] = action
     end
 
   end
