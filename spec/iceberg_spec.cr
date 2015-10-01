06ef0286bc0e10bd097dff352ab23212bc4fddf9
Adler <nkj20932@hotmail.com>
2015-10-01 14:28:10 +0800

    remove invalid tests

--- spec/iceberg_spec.cr
+++ spec/iceberg_spec.cr
@@ -1,19 +1,16 @@
 require "./spec_helper"
 
 App = Iceberg::App.new
-App.get "/app" do
-  "this is app page"
-end
 
 describe Iceberg::App do
-  describe "#new" do
-    it "- no error" do
-      App.should be_truthy
-    end
-  end
-  describe "#get" do
-    it "setup routes" do
-      App.route.map["/app"].should be_truthy
-    end
-  end
+  # describe "#new" do
+  #   it "- no error" do
+  #     App.should be_truthy
+  #   end
+  # end
+  # describe "#get" do
+  #   it "setup routes" do
+  #     App.route.map["/app"].should be_truthy
+  #   end
+  # end
 end
